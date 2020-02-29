b765402b44780c455e1fe9dee31eb9dcf3817e07
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 13:32:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,7 +40,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    new_dog = self.new(row[1], row[2], row[0])
+    new_dog = self.new(name: row[1], breed :row[2], id: row[0])
     new_dog
   end
 end
