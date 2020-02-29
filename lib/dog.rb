427b368c9b53bb8de29cc2b5db4802753225e138
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 13:43:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,5 +66,15 @@ class Dog
       end
    end
    
-   
+   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+     SQL
+      DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
