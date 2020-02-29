a2ba87c3fee02e387dbed7b7fa44e8a23bd6defb
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 13:12:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,7 +21,7 @@ class Dog
   
   def save
     sql = <<-SQL
-      INSERT INTO dogss (name, breed) 
+      INSERT INTO dogs (name, breed) 
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
