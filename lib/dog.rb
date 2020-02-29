b1b191349161f80dc3f72c900f9d7af0451de710
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-29 12:53:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,11 @@ class Dog
     @name = name
     @breed = breed
   end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXIST dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
